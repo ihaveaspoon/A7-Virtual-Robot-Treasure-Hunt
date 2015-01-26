70ab870348c66606adf2bc06fea938d8dc075081
Alvin So <alvinso@hotmail.co.uk>
2015-01-26 17:06:00 +0000

    Update Treasure Hunt - Level One.py

--- Treasure Hunt - Level One.py
+++ Treasure Hunt - Level One.py
@@ -11,18 +11,14 @@ from time import time
 from math import floor
 from pygame.locals import *
 
-
-
-
 #Initialise Pygame/canvas
 pygame.init()
 pygame.display.set_caption('Treasure Hunt - Level One')
 screen = pygame.display.set_mode([640,580])
 white = (255,255,255)
-screen.fill(white)                      #make it in to white screen
+screen.fill(white)    #Set background white
 
-
-#Creating colours
+#Creating colours for use
 white = (255,255,255)
 blue = (176,224,230)
 dblue = (0,129,242)
@@ -46,7 +42,7 @@ pygame.draw.rect(screen,blue,stop)
 #Create a Start screen
 class start_button:
 
-    def __intit__(self,x,y):
+    def __intit__(self,x,y): #Initiliase
         self.button= pg.key.get_pressed()
         self.x=x
         self.y=y
@@ -56,9 +52,7 @@ class start_button:
         self.quit=pygame.quit()
         self.sys=sys.exit()
         
-
-
-    def loop(self):
+    def loop(self): #Loops
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -70,9 +64,8 @@ class start_button:
                         self.quit=pygame.quit()
                         self.sys=sys.exit()
                     return
-   
 
-    def drawText(self,text, font, surface, x, y,TEXTCOLOR):
+    def drawText(self,text, font, surface, x, y,TEXTCOLOR): #Create the starting text
             self.x=x
             self.y=y
             self.color=TEXTCOLOR
@@ -81,12 +74,10 @@ class start_button:
             self.textrect.topleft = (self.x,self.y)
             surface.blit(self.textobj, self.textrect)
 
-
-
 #Creating class for Car
 class Car:
     global screen
-    def __init__(self, x, y):
+    def __init__(self, x, y): # Initialising Images
         self.car_img = "player_car.png"
         self.car_imgt = "player_cart.png"
         self.car_imgb = "player_carb.png"
@@ -94,7 +85,7 @@ class Car:
         self.x = x
         self.y = y
 
-    def create(self):
+    def create(self): #Function used to create the robot
         self.car_load = pygame.image.load(self.car_img)
         self.car_loadt = pygame.image.load(self.car_imgt)
         self.car_loadb = pygame.image.load(self.car_imgb)
@@ -102,8 +93,7 @@ class Car:
         self.car      = self.car_load.get_rect()
         self.car.x = self.x
         self.car.y = self.y
-        
-        
+        #Functions for rotations of the car
     def car_update(self):
         screen.blit(self.car_load, self.car)
     def car_updatet(self):
@@ -117,65 +107,66 @@ class Car:
         sleep(0.05)
 class Landmark: #Creating class for Landmarks
     global screen
-    def __init__(self, x, y):
+    def __init__(self, x, y): # Initilasing
         self.landmark_img = "traffic_cone.png"
         self.x = x
         self.y = y
 
-    def create(self):
+    def create(self): #Function used to create landmarks
         self.landmark_load = pygame.image.load(self.landmark_img)
         self.landmark      = self.landmark_load.get_rect()
         self.landmark.x = self.x
         self.landmark.y = self.y
 
-    def landmark_update(self):
+    def landmark_update(self): # Function used to update the landmarks onto the screen
         screen.blit(self.landmark_load, self.landmark)
 
-class Red:
+class Red: # Class for Red Traffic light
     global screen
     def __init__(self, x, y):
         self.red_img = "red.png"
         self.x = x
         self.y = y
 
-    def create(self):
+    def create(self): # Function used to create red light
         self.red_load=pygame.image.load(self.red_img)
         self.red=self.red_load.get_rect()
         self.red.x=self.x
         self.red.y=self.y
 
-    def red_update(self):
+    def red_update(self):# Function used to update red light
         screen.blit(self.red_load,self.red)
 
-class Yellow:
+class Yellow: #Class for yellow light
     global screen
-    def __init__(self, x, y):
+    def __init__(self, x, y): # Initialise image
         self.yellow_img = "yellow.png"
         self.x = x
         self.y = y
 
-    def create(self):
+    def create(self): #Function used to create yellow light
         self.yellow_load=pygame.image.load(self.yellow_img)
         self.yellow=self.yellow_load.get_rect()
         self.yellow.x=self.x
         self.yellow.y=self.y
-    def yellow_update(self):
+    def yellow_update(self):#Function used to update yellow light
         screen.blit(self.yellow_load,self.yellow)
 
-class Green:
+class Green: # Class for green light
     global screen
-    def __init__(self, x, y):
+    def __init__(self, x, y): # Initialise image
         self.green_img = "green.png"
         self.x = x
         self.y = y
 
-    def create(self):
+    def create(self): # Function used to create green light
         self.green_load=pygame.image.load(self.green_img)
         self.green=self.green_load.get_rect()
         self.green.x=self.x
         self.green.y=self.y
-    def green_update(self):
+    def green_update(self):#Function used to update green light
         screen.blit(self.green_load,self.green)
+#Functions used for the rotations of the car
 def update():
     screen.blit(bg, background_position)
     car.car_update()
@@ -255,7 +246,7 @@ car = Car(250,380)
 car.create()
 update()
 
-#Landmarks
+#Creating instances of Landmarks
 l1 = Landmark(230,35)
 l1.create()
 l2 = Landmark(360,150)
@@ -265,7 +256,7 @@ l3.create()
 l4 = Landmark(530,400)
 l4.create()
 
-#Traffic Lights
+#Creating instances of traffic lights
 yellow=Yellow(45,490)
 yellow.create()
 green=Green(45,490)
@@ -274,14 +265,13 @@ red=Red(45,490)
 red.create()
 update()
 
-# show the start screen
+#Show the start screen
 TW=start_button()
 TW.drawText('Treasure Hunt!', font, screen,150,220,(255,0,0))
 TW.drawText('Press any key to start.', font, screen, 80, 300,(255,255,255))
 pygame.display.update()
 TW.loop()
-
-#test
+#Timer Run
 running = True
 timerOn = 0 # Start timer flag
 #Movement and timing
